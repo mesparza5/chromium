d6d8d58ad0fc36c23aae22748d672171a9cd2f3d
Mike Esparza <mikeesparza5@gmail.com>
2020-09-30 10:01:58 -0700

    Fixing look on the basic template

--- themes/chromium/full.php
+++ themes/chromium/full.php
@@ -2,12 +2,14 @@
 defined('C5_EXECUTE') or die("Access Denied.");
 $this->inc('elements/header.php'); ?>
 
-<main>
-    <?php
-    $a = new Area('Main');
-    $a->enableGridContainer();
-    $a->display($c);
-    ?>
-</main>
+<div class="cms-page">
+    <main>
+        <?php
+        $a = new Area('Main');
+        $a->enableGridContainer();
+        $a->display($c);
+        ?>
+    </main>
+</div>
 
-<?php   $this->inc('elements/footer.php'); ?>
+<?php $this->inc('elements/footer.php'); ?>
